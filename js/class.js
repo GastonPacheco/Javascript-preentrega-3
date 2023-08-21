f95d9d6a59d0bcac8de695418f528556d14da091
Gaston Pacheco <cgastonpacheco@gmail.com>
2023-08-20 23:32:34 -0300

    entregafinal

--- js/class.js
+++ js/class.js
@@ -13,7 +13,7 @@ class Mates {
 
 const mate1 = new Mates (1, "Mate", "Cactus", 3500, "mates1.jpg")
 const mate2 = new Mates (2, "Mate", "Buda", 4500, "mates2.jpg")
-const mate3 = new Mates (3, "Mate", "Storm Trooper", 3800, "mates3.jpg")
+const mate3 = new Mates (3, "Mate", "Storm Trooper", 3700, "mates3.jpg")
 const mate4 = new Mates (4, "Mate", "Pokebola", 2000, "mates4.jpg")
 const mate5 = new Mates (5, "Mate", "Harry Potter", 5000, "mates5.jpg")
 const mate6 = new Mates (6, "Mate", "Stitch", 4000, "mates6.jpg")
